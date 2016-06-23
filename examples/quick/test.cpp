0d9adaefaff6e7c289651d2ccc40bac350979868
Marc-Olivier Barre <mobarre@libon.com>
2016-06-23 18:40:51 +0200

    Make use of our new cool << operator overload for elements and params

--- examples/quick/test.cpp
+++ examples/quick/test.cpp
@@ -9,77 +9,36 @@ int main (int argc, char* argv[])
 {
   /** Alternative test, with an initial param list at t0 */
   mKOST::Elements elements;
-  elements.a = 5.799119e5;
-  elements.Ecc = 0.5;
-  elements.i = SIMD_PI / 4;
-  elements.LoP = SIMD_PI / 4;
+  elements.a = 5006.2798222;
+  elements.Ecc = 0.997491222;
+  elements.i = 0.0;
+  elements.LoP = -SIMD_HALF_PI;
   elements.LAN = 0.0;
-  elements.L = SIMD_HALF_PI;
 
   mKOST::Orbit orbit (MU, elements);
 
-  /** Convert to orbital elements */
-  mKOST::StateVectors out = orbit.elements2StateVector (elements.L, SIMD_EPSILON, 1000000);
-
-  printf ("Orbital elements:\n"
-          "     a = %f m\n"
-          "     e = %f\n"
-          "     i = %f\n"
-          " theta = %f\n"
-          "omegab = %f\n"
-          "   MeL = %f\n",
-          elements.a, elements.Ecc, elements.i, elements.LAN, elements.LoP, out.MeL
-         );
-
+  mKOST::StateVectors out = orbit.elements2StateVector (SIMD_HALF_PI, SIMD_EPSILON, 1000000);
+  printf ("state:\n"
+          "  position: %f, %f, %f\n"
+          "  velocity: %f, %f, %f\n",
+          out.pos.getX(),
+          out.pos.getY(),
+          out.pos.getZ(),
+          out.vel.getX(),
+          out.vel.getY(),
+          out.vel.getZ()
+          );
+  std::cout << elements << std::endl;
   mKOST::Params params (orbit.getParams());
-  printf ("Additional parameters:\n"
-          "   PeD = %f m\n"
-          "   ApD = %f m\n"
-          "   MnA = %f\n"
-          "   TrA = %f\n"
-          "   EcA = %f\n"
-          "     T = %f s\n"
-          "   AgP = %f\n",
-          params.PeD,
-          params.ApD,
-          params.MnA,
-          params.TrA,
-          params.EcA,
-          params.T,
-          params.AgP
-         );
+  std::cout << params << std::endl;
 
   orbit.refreshFromStateVectors (&out);
-  mKOST::Elements elements2 (orbit.getElements());
-  printf ("Orbital elements reversed:\n"
-          "     a = %f m\n"
-          "     e = %f\n"
-          "     i = %f\n"
-          " theta = %f\n"
-          "omegab = %f\n"
-          "     L = %f\n",
-          elements2.a, elements2.Ecc, elements2.i, elements2.LAN, elements2.LoP, elements2.L
-         );
-
+  elements = orbit.getElements();
+  std::cout << elements << std::endl;
   params = orbit.getParams();
-  printf ("Additional parameters:\n"
-          "   PeD = %f m\n"
-          "   ApD = %f m\n"
-          "   MnA = %f\n"
-          "   TrA = %f\n"
-          "   EcA = %f\n"
-          "     T = %f s\n"
-          "   AgP = %f\n",
-          params.PeD,
-          params.ApD,
-          params.MnA,
-          params.TrA,
-          params.EcA,
-          params.T,
-          params.AgP
-         );
+  std::cout << params << std::endl;
 
-  out = orbit.elements2StateVector(out.MeL, SIMD_EPSILON, 1000);
+  out = orbit.elements2StateVector(SIMD_HALF_PI, SIMD_EPSILON, 1000);
   printf ("state:\n"
           "  position: %f, %f, %f\n"
           "  velocity: %f, %f, %f\n",
